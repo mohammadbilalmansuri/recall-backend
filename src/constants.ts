f29fd2b317f468ef2f4c73fb82ac6be23c5cb881
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2025-03-12 17:29:17 +0530

    feat: update constants for improved type handling and add Pinecone and Gemini API configurations

--- src/constants.ts
+++ src/constants.ts
@@ -1,10 +1,16 @@
-export const PORT = process.env.PORT || 8000;
-export const DB_NAME = "recalldb";
-export const MONGODB_URI = process.env.MONGODB_URI;
-export const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET;
-export const ACCESS_TOKEN_EXPIRY = process.env.ACCESS_TOKEN_EXPIRY || "1d";
-export const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET;
-export const REFRESH_TOKEN_EXPIRY = process.env.REFRESH_TOKEN_EXPIRY || "7d";
+export const PORT = Number(process.env.PORT) || 8000;
+export const MONGODB_CONNECTION_STRING = `${process.env.MONGODB_URI}/${process.env.MONGODB_NAME}`;
+
+export const PINECONE_API_KEY = String(process.env.PINECONE_API_KEY);
+export const PINECONE_INDEX_NAME = String(process.env.PINECONE_INDEX_NAME);
+
+export const ACCESS_TOKEN_SECRET = String(process.env.ACCESS_TOKEN_SECRET);
+export const ACCESS_TOKEN_EXPIRY =
+  String(process.env.ACCESS_TOKEN_EXPIRY) || "1d";
+export const REFRESH_TOKEN_SECRET = String(process.env.REFRESH_TOKEN_SECRET);
+export const REFRESH_TOKEN_EXPIRY =
+  String(process.env.REFRESH_TOKEN_EXPIRY) || "7d";
+
 export const COOKIE_OPTIONS = {
   httpOnly: true,
   // secure: true,
@@ -12,3 +18,5 @@ export const COOKIE_OPTIONS = {
 };
 // export const ALLOWED_ORIGINS =
 //   process.env.ALLOWED_ORIGINS?.split(",").map((origin) => origin.trim()) || [];
+
+export const GEMINI_API_KEY = String(process.env.GEMINI_API_KEY);
