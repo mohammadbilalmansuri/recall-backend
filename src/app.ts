337da2d09ed27e5ef728fff1b85d71de7dd16cb4
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2025-03-17 15:41:26 +0530

    feat: add static file serving for public directory

--- src/app.ts
+++ src/app.ts
@@ -12,6 +12,7 @@ app.use(
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(cookieParser());
+app.use(express.static("public"));
 
 import authRoutes from "./routes/auth.routes";
 import contentRoutes from "./routes/content.routes";
