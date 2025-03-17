2af9d2797ca25df831f3c19a0f385c07e43b5e97
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2025-03-17 15:42:17 +0530

    refactor: update Pinecone index variable for improved clarity

--- src/db/pinecone.db.ts
+++ src/db/pinecone.db.ts
@@ -2,9 +2,9 @@ import { Pinecone } from "@pinecone-database/pinecone";
 import { PINECONE_API_KEY, PINECONE_INDEX_NAME } from "../constants";
 
 const pc = new Pinecone({
-  apiKey: PINECONE_API_KEY as string,
+  apiKey: PINECONE_API_KEY,
 });
 
-const index = pc.index(PINECONE_INDEX_NAME as string);
+const pinconeIndex = pc.index(PINECONE_INDEX_NAME);
 
-export default index;
+export default pinconeIndex;
