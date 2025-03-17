9b641151e8c6573037b40ce95c8627ddf37ea2b0
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2025-03-17 15:42:51 +0530

    feat: integrate multer middleware for PDF file upload in content routes

--- src/routes/content.routes.ts
+++ src/routes/content.routes.ts
@@ -7,10 +7,11 @@ import {
 } from "../controllers/content.controllers";
 import authMiddleware from "../middlewares/auth.middleware";
 import validateContent from "../middlewares/content.middleware";
+import upload from "../middlewares/multer.middleware";
 
 const contentRoutes = Router();
 
-contentRoutes.post("/", authMiddleware, addContent);
+contentRoutes.post("/", authMiddleware, upload.single("pdf"), addContent);
 contentRoutes.get("/:id", authMiddleware, validateContent, getContent);
 contentRoutes.delete("/:id", authMiddleware, validateContent, deleteContent);
 contentRoutes.get("/", authMiddleware, fetchContents);
