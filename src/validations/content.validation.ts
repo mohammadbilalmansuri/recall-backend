e40f887f0c397786c3c6d4c6f73562d2a325cd8f
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2025-03-18 16:38:52 +0530

    refactor: update validation messages for clarity and consistency in content and user validations

--- src/validations/content.validation.ts
+++ src/validations/content.validation.ts
@@ -2,10 +2,10 @@ import { z } from "zod";
 import { ContentType } from "../models/content.model";
 
 export const contentValidation = z.object({
-  text: z
+  description: z
     .string()
-    .min(5, "Text must be at least 5 characters long.")
-    .max(2000, "Text must not exceed 2000 characters."),
+    .min(5, "Description must be at least 5 characters long")
+    .max(2000, "Description must not exceed 2000 characters"),
   link: z.string().url("Please enter a valid URL").optional(),
   type: z.nativeEnum(ContentType),
   tags: z.array(z.string()).optional(),
--- src/validations/user.validation.ts
+++ src/validations/user.validation.ts
@@ -5,15 +5,15 @@ const nameValidation = z
   .min(3, "Name must be at least 3 characters long")
   .max(50, "Name must not exceed 50 characters");
 
-const emailValidation = z.string().email("Please enter a valid email address.");
+const emailValidation = z.string().email("Please enter a valid email address");
 
 const passwordValidation = z
   .string()
-  .min(8, "Password must be at least 8 characters long.")
-  .max(24, "Password must not exceed 24 characters.")
+  .min(8, "Password must be at least 8 characters long")
+  .max(24, "Password must not exceed 24 characters")
   .regex(
     /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[\W_]).*$/,
-    "Password must include at least one lowercase letter, one uppercase letter, one number, and one special character."
+    "Password must include at least one lowercase letter, one uppercase letter, one number, and one special character"
   );
 
 export const signupUserValidation = z.object({
