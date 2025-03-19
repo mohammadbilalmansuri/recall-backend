6b27905bfae9e9e65a71d7652befc9de17e76472
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2025-03-19 10:09:30 +0530

    refactor: remove multer and cloudinary services, update content routes to use verifyAccess middleware

--- src/routes/content.routes.ts
+++ src/routes/content.routes.ts
@@ -5,15 +5,19 @@ import {
   deleteContent,
   fetchContents,
 } from "../controllers/content.controllers";
-import authMiddleware from "../middlewares/auth.middleware";
+import verifyAccess from "../middlewares/auth.middleware";
 import validateContent from "../middlewares/content.middleware";
-import upload from "../middlewares/multer.middleware";
 
 const contentRoutes = Router();
 
-contentRoutes.post("/", authMiddleware, upload.single("pdf"), addContent);
-contentRoutes.get("/:id", authMiddleware, validateContent, getContent);
-contentRoutes.delete("/:id", authMiddleware, validateContent, deleteContent);
-contentRoutes.get("/", authMiddleware, fetchContents);
+contentRoutes.post("/add", verifyAccess, addContent);
+contentRoutes.get("/get/:id", verifyAccess, validateContent, getContent);
+contentRoutes.delete(
+  "/delete/:id",
+  verifyAccess,
+  validateContent,
+  deleteContent
+);
+contentRoutes.get("/all", verifyAccess, fetchContents);
 
 export default contentRoutes;
