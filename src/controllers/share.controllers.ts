8cc79fc2b9731e8947cb9ac09d37ea76e8e45c82
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2025-03-21 21:24:05 +0530

    refactor: enhance API response structure in share link management

--- src/controllers/share.controllers.ts
+++ src/controllers/share.controllers.ts
@@ -10,31 +10,28 @@ export const getShareLink = asyncHandler(async (req, res) => {
 
   const share = await Share.create({ owner: req.user?.id });
 
-  return new ApiResponse(201, "Share link created successfully.", share).send(
-    res
-  );
+  return new ApiResponse(res, 201, "Share link created successfully.", {
+    share,
+  }).send();
 });
 
 export const getSharedContent = asyncHandler(async (req, res) => {
   const share = await Share.findOne({ hash: req.params.hash });
   if (!share) throw new ApiError(404, "Invalid share link.");
 
-  const content = await Content.find({ owner: share.owner }).populate({
+  const contents = await Content.find({ owner: share.owner }).populate({
     path: "owner",
     select: "name email",
   });
 
-  return new ApiResponse(
-    200,
-    "Shared Content fetched successfully.",
-    content
-  ).send(res);
+  return new ApiResponse(res, 200, "Shared Content fetched successfully.", {
+    contents,
+  }).send();
 });
 
 export const deleteShareLink = asyncHandler(async (req, res) => {
   const share = await Share.findOneAndDelete({ owner: req.user?.id });
-
   if (!share) throw new ApiError(404, "Share link not found.");
 
-  return new ApiResponse(200, "Share link deleted successfully.").send(res);
+  return new ApiResponse(res, 200, "Share link deleted successfully.").send();
 });
