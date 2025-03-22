d504aae710eac70789f8fae19d45ee9db445f527
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2025-03-22 06:06:47 +0530

    refactor: streamline content retrieval logic and improve context formatting in AI controller

--- src/controllers/ai.controllers.ts
+++ src/controllers/ai.controllers.ts
@@ -1,6 +1,5 @@
 import asyncHandler from "../utils/asyncHandler";
 import ApiError from "../utils/ApiError";
-import Content from "../models/content.model";
 import validate from "../utils/validate";
 import { promptValidation } from "../validations/content.validation";
 import pineconeIndex from "../db/pinecone.db";
@@ -17,22 +16,11 @@ export const askAIAboutSpecificContent = asyncHandler(async (req, res) => {
   const { prompt } = validate(promptValidation, req.body);
   if (!prompt) throw new ApiError(400, "Prompt is required");
 
-  const savedData = await pineconeIndex
+  const content = await pineconeIndex
     .namespace(`user_${req.user?.id}`)
-    .searchRecords({
-      query: {
-        topK: 1,
-        inputs: { text: prompt },
-      },
-      fields: ["text", "category"],
-    });
+    .fetch(req.content?.id);
 
-  const context = savedData.result?.hits
-    .map((hit) => {
-      const fields = hit.fields as { text?: string };
-      return fields.text || "No text available.";
-    })
-    .join("\n\n");
+  const context = `${content.records?.[req.content?.id]?.metadata?.text}`;
 
   await generateAIResponseStream(prompt, context, res);
 });
@@ -53,12 +41,11 @@ export const askAIWithSavedContext = asyncHandler(async (req, res) => {
 
   const hits = savedData.result?.hits || [];
   const context = hits
-    .map((hit, index) => {
-      const fields = hit.fields as { text?: string };
-      return `### Context ${index + 1}:\n${
-        fields.text || "No text available."
-      }`;
-    })
+    .map(
+      (hit: { fields: { text?: string } }, index) =>
+        `### Context ${index + 1}:\n${hit.fields?.text}`
+    )
+    .filter(Boolean)
     .join("\n\n");
 
   await generateAIResponseStream(prompt, context, res);
